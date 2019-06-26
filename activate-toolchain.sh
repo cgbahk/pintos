85d07d17a586b1daaa89a8a41f5f13fa9dfabece
CG Bahk <cg.bahk@gmail.com>
2019-06-26 17:35:07 -0400

    Activate script append utils dir to PATH

--- activate-toolchain.sh
+++ activate-toolchain.sh
@@ -3,7 +3,12 @@
 
 SRC_RPATH=$(dirname ${BASH_SOURCE[0]})
 SRC_PATH=$(cd $SRC_RPATH && pwd)
-TOOLCHAIN_BIN_PATH=$SRC_PATH/toolchain/x86_64/bin
 
-export PATH=$TOOLCHAIN_BIN_PATH:$PATH
+TOOLCHAIN_BIN_PATH=$SRC_PATH/toolchain/x86_64/bin
+UTILS_PATH=$SRC_PATH/src/utils
+
+PATH=$TOOLCHAIN_BIN_PATH:$PATH
+PATH=$UTILS_PATH:$PATH
+export PATH
+
 export PS1="${PS1:: -2}(i386-elf) > "
